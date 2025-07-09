4defeb2b556414ba24bd076b9c6338a384e8af74
Adam Sedláček <sedlaad1@fit.cvut.cz>
2025-07-09 17:21:46 +0200

    Adds tests for BigInt::from()

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,22 @@ mod tests {
         assert_eq!(new.positive, true);
         assert_eq!(new.numbers, [0].to_vec());
     }
+
+    #[test]
+    fn from() {
+        let x = BigInt::from(20);
+        assert_eq!(x.positive, true);
+        assert_eq!(x.numbers, [2, 0].to_vec());
+        let x = BigInt::from(-20);
+        assert_eq!(x.positive, false);
+        assert_eq!(x.numbers, [2, 0].to_vec());
+        let x = BigInt::from(-320020000981234567890);
+        assert_eq!(x.positive, false);
+        assert_eq!(
+            x.numbers,
+            [3, 2, 0, 0, 2, 0, 0, 0, 0, 9, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0].to_vec()
+        );
+    }
 }
 
 fn main() {}
